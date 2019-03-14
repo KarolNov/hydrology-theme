746480d7cd5388690b93d7cf5cb4a98ae17f5ee9
KarolNov <karolnovljakovic@gmail.com>
2019-03-14 11:59:19 +0100

    working on mobile website navigation and main template

--- demo/index.js
+++ demo/index.js
@@ -1,16 +1,42 @@
 
-window.onload = ()=>{
+window.onload = () => {
+    setTimeout(()=>{
+        let loader = document.getElementById("loading");
+        loader.style.display = "none";
+    }, 2000);
 
-    const collapse = Array.from(document.getElementsByClassName('collapse'));
-
-    collapse.forEach(el=>{
-        el.onclick = (e)=>{
-            //show proper side page
+    const nav = Array.from(document.getElementsByClassName('nav'));
+    nav.forEach(el => {
+        el.onclick = (e) => {
+            //if id is back then show main page
+            console.log(e.target.id);
             let targetId = e.target.id;
-            document.getElementById(`${targetId}_content`).style.display = "block";
-            //animate transition between pages
-            document.getElementById('mainPage').className = 'hidden';
-            document.getElementById('sidePage').className = 'show';
+            if (targetId === "back"){
+                //animate transition between pages
+                document.getElementById('mainPage').className = 'shown';
+                document.getElementById('sidePage').className = 'hidden';
+            }else{
+                //hide previously shown side page
+                let sidePages = Array.from(document.getElementsByClassName('sidePageContent'));
+                sidePages.forEach(page=>{
+                    page.style.display = "none";
+                })
+                //show proper side page
+                document.getElementById(`${targetId}_content`).style.display = "block";
+                document.getElementById(`${targetId}_content`).style.display = "block";
+                //animate transition between pages
+                document.getElementById('mainPage').className = 'hidden';
+                document.getElementById('sidePage').className = 'shown';
+                //if there is a table to show inform user about scroll feature
+                if(targetId==="publications"){
+                    let table = document.getElementById("publicationsTable");
+                    //check if scroll is needed
+                    if(table.parentElement.offsetWidth<table.offsetWidth){
+                        console.log("scroll needed")
+                    }
+                }
+            }
         };
     })
+
 }
